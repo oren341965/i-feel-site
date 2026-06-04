13787f93576ae9c34bc8a77927fecbc3e89fb605
oren341965 <oren@i-feel.co.il>
2026-06-05 01:55:27 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://oren341965.github.io',
-  base: '/i-feel-site',
+  site: 'https://www.i-feel.co.il',
   integrations: [],
   devToolbar: { enabled: false },
   output: 'static'
