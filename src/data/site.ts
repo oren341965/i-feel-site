2f3c4b530b6dbbe8df007a2c80286d783545d756
oren341965 <oren@i-feel.co.il>
2026-06-09 20:59:46 +0300

    Fix service phone number

--- src/data/site.ts
+++ src/data/site.ts
@@ -3,7 +3,7 @@ export const site = {
   url: 'https://i-feel.co.il',
   phone: '03-508-9553',
   whatsappMaya: '053-345-0205',
-  whatsappService: '053-348-1342',
+  whatsappService: '053-300-6239',
   email: 'sales@i-feel.co.il',
   supportEmail: 'support@i-feel.co.il',
   address: 'אימבר 14, פתח תקווה 4951148'
