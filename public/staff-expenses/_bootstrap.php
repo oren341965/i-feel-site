2390c99f5ffcb77079566969b0840e1f96735c19
oren341965 <oren@i-feel.co.il>
2026-07-29 03:46:17 +0300

    Polyfill PHP 8 string helpers

--- public/staff-expenses/_bootstrap.php
+++ public/staff-expenses/_bootstrap.php
@@ -1,6 +1,20 @@
 <?php
 declare(strict_types=1);
 
+if (!function_exists('str_starts_with')) {
+    function str_starts_with(string $haystack, string $needle): bool
+    {
+        return $needle === '' || substr($haystack, 0, strlen($needle)) === $needle;
+    }
+}
+
+if (!function_exists('str_contains')) {
+    function str_contains(string $haystack, string $needle): bool
+    {
+        return $needle === '' || strpos($haystack, $needle) !== false;
+    }
+}
+
 const IFEEL_PORTAL_VERSION = '1.5.0';
 const IFEEL_PORTAL_SESSION = 'ifeel_staff_expenses';
 const IFEEL_PORTAL_IDLE_TIMEOUT = 3600;
